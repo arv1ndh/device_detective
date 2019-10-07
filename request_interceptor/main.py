710cee25a85a6b4bdb8c63b52a57dba97ffa67af
Arvindh Kumar Chandran <arvindhium@gmail.com>
2019-10-06 18:26:36 -0700

    Extracted the ip address as well

--- request_interceptor/main.py
+++ request_interceptor/main.py
@@ -9,5 +9,6 @@ def hello_world():
 @app.route("/ua")
 def useragent_extractor():
     ua = request.headers.get("user-agent")
-    display_message = f"The User Agent is {ua}"
+    ip = request.remote_addr
+    display_message = f"The User Agent of {ip} is {ua}"
     return display_message
