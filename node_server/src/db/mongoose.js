6e053b9eef8b05aaadc50a863d1d37d84e66973c
Alex Hussar <alexandrahussar01@gmail.com>
2019-10-18 16:24:21 -0700

    debug logging

--- node_server/src/db/mongoose.js
+++ node_server/src/db/mongoose.js
@@ -4,9 +4,8 @@ require('dotenv').config()
 
 
 var my_URI = process.env.MONGODB_URI;
-const DB_NAME = 'NetworkDevices'
-const COLLECTION_NAME = 'devices'
 
+console.log('mongoose file')
 //connect to database
 mongoose.connect(my_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
 .then(()=> {console.log("MongoDB Connected...")})
