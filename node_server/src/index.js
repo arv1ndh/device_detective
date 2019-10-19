49fe628cb9f9041391d3d87738f78c7a6b19fa7f
Alex Hussar <alexandrahussar01@gmail.com>
2019-10-18 17:14:31 -0700

    heroku is up and running. Logging messages through heroku implemented

--- node_server/src/index.js
+++ node_server/src/index.js
@@ -36,16 +36,10 @@ app.get('/devices', async (req, res) =>{
         console.log('Successfully searched for all devices')
         res.send(devices)
     } catch (error) {
-        console.log('Error occured')
+        console.log('Error occured: ' + error)
         res.status(500).send()
     }
 
-    
-    // Device.find({}).then((devices) => {
-    //     res.send(devices)
-    // }).catch((e)=>{
-    //     res.status(500).send()
-    // })
 })
 
 //fetch specific type of device (mobile, tablet or pc)
@@ -53,23 +47,17 @@ app.get('/devices/:type', async (req, res) =>{
     const _type = req.params.type
    
     try {
+        console.log('Attempting search for devices of type: '+ _type)
         const devices = await Device.find({type: _type})
         if(devices.length == 0){
+            console.log('There were no devices of type "'+_type+'".')
             return res.status(404).send('There were no devices of type "'+_type+'".')
         }
         res.send(devices)
     } catch (error) {
+        console.log('Error occured: ' + error)
         res.status(500).send()
     }
-
-    // Device.find({type: _type}).then((devices) => {
-    //     if (devices == null){
-    //         return res.status(404).send()
-    //     }
-    //     res.send(devices)
-    // }).catch((e)=>{
-    //     res.status(500).send()
-    // })
 })
 
 
