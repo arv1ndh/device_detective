dc41d38467bb55d457e22f5498b96a6397226a83
Arvindh Kumar Chandran <arvindhium@gmail.com>
2019-10-22 16:11:51 -0700

    Reading values from a conf file

--- request_interceptor/proxy.py
+++ request_interceptor/proxy.py
@@ -1,12 +1,15 @@
 import boto3
+import json
 from mitmproxy import http
 
-sqs = boto3.client(service_name = 'sqs',region_name = "us-west-1", aws_access_key_id = "AKIAJPNXAACALE34XPPA", aws_secret_access_key="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")
-QUEUE_URL = 'https://sqs.us-west-1.amazonaws.com/809875841865/ua_headers'
+with open("conf.json") as json_f:
+    data = json.load(json_f)
+
+sqs = boto3.client(service_name = data["service_name"],region_name = data["region_name"], aws_access_key_id = data["key_id"], aws_secret_access_key = data["access"])
 
 def request(flow: http.HTTPFlow):
     header_fields = flow.request.headers.fields
     for key,value in header_fields:
         if key == bytes("User-Agent",'utf-8'):
             ua_string = str(value, 'utf-8')
-            sent_ua = sqs.send_message(QueueUrl=QUEUE_URL, MessageBody=ua_string)
+            sent_ua = sqs.send_message(QueueUrl=data["q_url"], MessageBody=ua_string)
