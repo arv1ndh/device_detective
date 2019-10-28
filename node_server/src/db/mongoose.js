f4b7a3701797aeed3d55955b04898fb46200b274
Alex Hussar <alexandrahussar01@gmail.com>
2019-10-28 11:58:19 -0700

    Connected offical db

--- node_server/src/db/mongoose.js
+++ node_server/src/db/mongoose.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 require('dotenv').config()
 
 //heroku didnt like using an inermediate value. must put it directly into the mongoos.connect()
-//var my_URI = process.env.MONGODB_URI;
+var my_URI = process.env.MONGODB_URI;
 
 //connect to database
-mongoose.connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
+console.log('Attempting connection...')
+mongoose.connect("mongodb+srv://mobileapp:thisisatest@cluster0-kdfv3.mongodb.net/tests?retryWrites=true&w=majority", {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
 .then(()=> {console.log("MongoDB Connected...")})
 .catch((e) => console.log('There is an error here: '+e))
 
