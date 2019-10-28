0d2b3627a6b939f8ca3e01e1f9a4f2d4a621c4f7
chadbloxham <chadbloxham10@gmail.com>
2019-10-28 15:09:31 -0700

    DynDB table name no longer visible.

--- aws_lambda/lambda_function.py
+++ aws_lambda/lambda_function.py
@@ -8,12 +8,15 @@ import boto3
 def lambda_handler(event, context):
     dyn_client = boto3.client('dynamodb')
     body = event["Records"][0]["body"]
-    response = dyn_client.get_item(TableName='MACUA-Database', Key={'macua' : {'S' : body } } )
+    f = open("dynDB_table.txt", "r")
+    table = f.read()
+    f.close()
+    response = dyn_client.get_item(TableName=table, Key={'macua' : {'S' : body } } )
     try:
         match = response['Item']
     except:
         expire = int(time.time() + 3600*24*30)
-        response = dyn_client.put_item(TableName='MACUA-Database', Item={ 'macua' : { 'S' : body }, 'ttl' : { 'N' : str(expire) } })
+        response = dyn_client.put_item(TableName=table, Item={ 'macua' : { 'S' : body }, 'ttl' : { 'N' : str(expire) } })
         mac, delim, ua = body.partition('ua')
         parsed_ua = parse(ua)
         parsed_dict = make_dict(parsed_ua, mac)
