c8ef5cb20cdae70214bae3f7e05fa3c09b729535
Alex Hussar <alexandrahussar01@gmail.com>
2019-11-08 10:19:28 -0800

    set node.js to listen for changes to db

--- node_server/src/db/mongoose.js
+++ node_server/src/db/mongoose.js
@@ -7,7 +7,17 @@ var my_URI = process.env.MONGODB_URI;
 //connect to database
 console.log('Attempting connection...')
 mongoose.connect(my_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
-.then(()=> {console.log("MongoDB Connected...")})
+.then(()=> {
+    console.log("MongoDB Connected...")
+    const db = mongoose.connection;
+    const collection = db.collection("posts");
+    const changeStream = collection.watch();
+ // start listen to changes
+    changeStream.on("change", function(event) {
+    console.log('Change has occured');
+    console.log(JSON.stringify(event));
+    });
+})
 .catch((e) => console.log('There is an error here: '+e))
 
 
